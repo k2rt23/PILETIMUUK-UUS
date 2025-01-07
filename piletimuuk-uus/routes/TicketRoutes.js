a5c6b3381ba5fec1b64a5e8e861a20ebd03b66d5
LAPTOP-8BL927LI\kartt <karttriin@gmail.com>
2025-01-07 11:10:50 +0200

    no message

---
+++ piletimuuk-uus/routes/TicketRoutes.js
@@ -0,0 +1,12 @@
+const TicketsController = require('../controllers/TicketsController');
+
+module.exports = (app) => {
+    app.route('/tickets')
+        .get(TicketsController.getAll)
+        .post(TicketsController.create);
+
+    app.route('/tickets/:id')
+        .get(TicketsController.getById)
+        .put(TicketsController.editById)
+        .delete(TicketsController.deleteById);
+};
